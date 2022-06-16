1c82df59bdb7ade47ac0963ee3089f2219db094a
ninaschramm <schrammnina@outlook.com>
2022-06-16 06:21:57 -0300

    post tweet funcionando

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 import express from "express";
+import cors from 'cors';
 
-const server = express()
+const server = express();
 
-server.listen(3000)
+server.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
-server.get(" ")
+server.use(express.json())
+server.use(cors())
+
+server.use(function (request, response, next) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+
+const users = [];
+const tweets = []
+
+server.post('/sign-up', (request, response) => {
+  users.push(request.body);
+  response.send("OK")
+});
+
+server.post('/tweets', (request, response) => {
+    tweets.push(request.body);
+    response.send("OK")
+  })
+
+server.get('/tweets', (request, response) => {
+    response.send(tweets)
+  })
+
+
+
+server.listen(5000)
